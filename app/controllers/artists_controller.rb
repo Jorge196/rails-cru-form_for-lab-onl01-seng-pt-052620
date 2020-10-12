38834d53d657299db7a6c810eacc927700a01a67
Jorge Ramirez <joramirez96@gmail.com>
2020-10-12 22:04:20 +0000

    Automatically backed up by Learn

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,36 @@
+class ArtistsController < ApplicationController
+
+  # def self.controller_path
+  #   "artists"
+  # end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to artist_path(@artist)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  # def artist_params(*args)
+  #   params.require(:artist).permit(*args)
+  # end
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end 
+end
